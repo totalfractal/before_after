22438257852a936267e87e3de3e10edc66c338cf
Andrey Tyrtyshnikov <an.tyrtyshnikov@gmail.com>
2021-11-07 14:15:53 +0300

    migrate to null safety

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/andrey/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/andrey/FlutterProjects/before_after/example"
+export "FLUTTER_ROOT=/Users/andrej/src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/andrej/Documents/FlutterProjects/before_after/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
